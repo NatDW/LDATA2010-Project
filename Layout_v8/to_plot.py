047f60625e9de45038bb875cf8e6d022acebdce2
NatDW <37715951+NatDW@users.noreply.github.com>
2020-12-17 17:42:39 +0100

    good paths

--- Layout_v8/to_plot.py
+++ Layout_v8/to_plot.py
@@ -13,9 +13,10 @@ from bokeh.io import output_notebook, show, output_file
 from bokeh.models import ColumnDataSource, HoverTool, Panel
 from bokeh.models.widgets import Tabs
 
-os.environ['R_HOME'] = 'C:/Program Files/R/R-3.6.2'
-os.environ["PATH"] += os.pathsep + 'C:/Program Files/R/R-3.6.2/bin/x64/'
-os.environ["PATH"] += os.pathsep + 'C:/Program Files/R/R-3.6.2/'
+os.environ['R_HOME'] = "R-3.6.2"
+os.environ['R_USER'] = 'rpy2'
+os.environ["PATH"] += os.pathsep + "R-3.6.2/bin/x64/"
+os.environ["PATH"] += os.pathsep + "R-3.6.2/"
 
 import rpy2.robjects.packages as rpackages
 import rpy2.robjects as ro
